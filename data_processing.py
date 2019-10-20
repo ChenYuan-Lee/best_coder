2694135e97f08c51fa45c8d00071db319c2a1d2c
Wai Lun <suenwailun@gmail.com>
2019-10-20 09:50:33 +0800

    Add data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+def correlation_with_target(dataframe: pd.DataFrame, target_column_name: str):
+    # Compute Correlation between columns
+    for column in dataframe:
+        print(f'Correlation between {column} and target is {dataframe[target_column_name].corr(dataframe[column])}')
+
+def find_missing_data(dataframe: pd.DataFrame):
+    # Find out missing data percentage for columns which are missing data
+    for column in dataframe:
+        num_missing_data = dataframe[column].isna().sum()
+        len_data =len(dataframe)
+        if num_missing_data:
+            print(f'Mising data rate for {column} is {(num_missing_data/len_data*100).round(2)}%')
